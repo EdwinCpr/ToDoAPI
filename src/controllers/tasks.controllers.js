b0d5138872854ab35e766c65a3d2d75bb929e377
Edwin Echeverria <edwindanielnow@gmail.com>
2023-01-01 15:45:01 -0400

    add destroy

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -3,6 +3,7 @@ const Tasks = require("../models/tasks.models");
 const create = async (req, res) => {
     const newTask = req.body;
     newTask.userId = req.user.id;
+
     try {
         const task = await Tasks.create(newTask);
         res.status(201).json(task);
@@ -15,7 +16,7 @@ const getAll = async (req, res) => {
     const userId = req.user.id;
     try {
         const tasks = await Tasks.findOne({ where: { userId }, 
-        attributes: ["id", "title", "description"]
+        attributes: ["id", "title", "description", "completed"]
     });
         res.json(tasks);
     } catch (error) {
@@ -23,7 +24,49 @@ const getAll = async (req, res) => {
     };
 };
 
+const edit = async (req, res) => {
+    const { id } = req.params;
+    const { title, description, completed } = req.body;
+    const task = await Tasks.findByPk(id);
+
+    if(req.user.id !== task.userId) {
+        const error = new Error("No tienes permiso");
+        return res.status(400).json({ msg: error.message })
+    };
+
+    try {
+        const data = {
+            title: title || task.title,
+            description: description || task.description,
+            completed: completed || task.completed
+        };
+        await Tasks.update(data, { where: { id } });
+        res.json(data)
+    } catch (error) {
+        
+    };
+};
+
+const delet = async (req, res) => {
+    const { id } = req.params;
+    const task = await Tasks.findByPk(id);
+
+    if(req.user.id !== task.userId) {
+        const error = new Error("No tienes permiso");
+        return res.status(400).json({ msg: error.message })
+    };
+
+    try {
+        await Tasks.destroy({ where: { id }});
+        res.json({ msg: "Eliminado con exito" });
+    } catch (error) {
+        console.log(error);
+    };
+};
+
 module.exports = {
     create,
-    getAll
+    getAll,
+    edit,
+    delet
 };
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,9 +1,11 @@
 const Router = require("express");
-const { create, getAll } = require("../controllers/tasks.controllers");
+const { create, getAll, edit, delet } = require("../controllers/tasks.controllers");
 const chechAuth = require("../middlewares/checkAuth.middlewares");
 const router = Router();
 
 router.post("/tasks", chechAuth, create);
 router.get("/tasks", chechAuth, getAll)
+router.put("/tasks/:id", chechAuth, edit);
+router.delete("/tasks/:id", chechAuth, delet);
 
 module.exports = router;
