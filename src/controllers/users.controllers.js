156ba40ea9ed97343cb7f56a5fc1a91e5b50a20b
Edwin Echeverria <edwindanielnow@gmail.com>
2023-01-01 17:31:02 -0400

    edit createuser

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -19,7 +19,7 @@ const create = async (req, res) => {
 
     try {
         const user = await Users.create(newUser);
-        res.status(201).json({ msg: "Registro exitoso" });
+        res.status(201).json(user);
     } catch (error) {
         console.log(error);
     };
