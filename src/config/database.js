9bb85e4d0fc65316ca10962736e6eee494d77a2b
Edwin Echeverria <edwindanielnow@gmail.com>
2023-01-01 18:46:32 -0400

    change name

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const db = new Sequelize({
-    username: process.env.DB_USERNAME,
-    database: process.env.DB_DATABASE,
+    username: process.env.USER,
+    database: process.env.NAME,
     port: process.env.DB_PORT,
     host: process.env.HOST,
     password: process.env.DB_PASSWORD,
