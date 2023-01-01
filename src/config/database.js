2a377eb3c637e9e288025d37f7b14ef34e060f4e
Edwin Echeverria <edwindanielnow@gmail.com>
2023-01-01 17:47:23 -0400

    edit error host

--- src/config/database.js
+++ src/config/database.js
@@ -7,9 +7,10 @@ const db = new Sequelize({
     username: process.env.DB_USERNAME,
     database: process.env.DB_DATABASE,
     port: process.env.DB_PORT,
-    host: process.env.DB_LOCALHOST,
+    host: process.env.HOST,
     password: process.env.DB_PASSWORD,
-    dialect: "postgres"
+    dialect: "postgres",
+    logging: false
 });
 
 module.exports = db;
