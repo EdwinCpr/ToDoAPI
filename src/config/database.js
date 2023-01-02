a56b82b8d0935866a4f8581352321faeab9e10d4
Edwin Echeverria <edwindanielnow@gmail.com>
2023-01-01 20:02:21 -0400

    edit .env

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const db = new Sequelize({
-    username: process.env.USER || "postgres",
-    database: process.env.NAME || "todoapi",
+    username: process.env.DB_USER || "postgres",
+    database: process.env.DB_NAME || "todoapi",
     port: process.env.DB_PORT || "5432",
-    host: process.env.HOST || "localhost",
+    host: process.env.DB_HOST || "localhost",
     password: process.env.DB_PASSWORD || "root",
     dialect: "postgres",
     logging: false
